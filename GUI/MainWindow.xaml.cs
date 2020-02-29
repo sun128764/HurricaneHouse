d96d5c6972052d32ea0c81deb7bfc8756cd930c6
[sun128764] <[sun12764@gmail.com]>
2020-02-29 18:42:58 -0500

    add icons

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -25,18 +25,40 @@ namespace GUI
     {
         public MainWindow()
         {
-            InitializeComponent();
-            _ = new SeriesCollection{
+
+            SeriesCollection = new SeriesCollection
+            {
                 new LineSeries
                 {
-                    Values = new ChartValues<double> { 3, 5, 7, 4 }
-                    },
-                new ColumnSeries
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,7 }
+                },
+                new LineSeries
                 {
-                    Values = new ChartValues<decimal> { 5, 6, 2, 7 }
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 }
                 }
             };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            SeriesCollection.Add(new LineSeries
+            {
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0 //straight lines, 1 really smooth lines
+            });
+
+            //modifying any series values will also animate and update the chart
+            SeriesCollection[2].Values.Add(5d);
+
+            DataContext = this;
         }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
 
         private void SelectionChanged(object sender, RoutedPropertyChangedEventArgs<Object> e)
         {
