ab7775632e8e3cc98450b41245e2da37c74eb194
[sun128764] <[sun12764@gmail.com]>
2020-04-24 14:05:30 -0400

    Update SciChart Key

--- GUI/App.xaml.cs
+++ GUI/App.xaml.cs
@@ -9,7 +9,8 @@ namespace GUI
     {
         public App()
         {
-            SciChart.Charting.Visuals.SciChartSurface.SetRuntimeLicenseKey("lsD3ZavgutfDGYDC5C2TMelxHrCNU1deISxW5gR7qAdrUlegBXyN+Kmk3W/JmkDs2YQlVs479r3Aj8/kfW4izaLcrNvoSmfjAl8a3nMxoApVLVATpBw6ZNkB0NG3jFxAhbO6ite/bFP/6bO59ro5rbEE4ID6Lvyhyg9LH791q1osCRZKcu48DOvAQejKlaZBJw9VGrORF39jyDUUlyTjnEWhYsZjF5ON7iS9QeSzdjd8fbBTNtdMuzghHVkTLVPa6RJdW4Dzh5Ozby4H6bRw2RwDUy9q5UKFdnozo3fKxLwEGZ+lSyrPsE5Bv1uN9vM3O5e2nSDS+RlXR8mYK4O48hwiCIAdvoquaUGORcTSYoMnp7U2uaQxvCz46CMG/cDIpEtIbrcoGOXWYyEcHZa3xHPRJhEY9h6sXfnzIKm2oqMnRhRMXeDjNbaNwxCD0+KGTW7o9kBj0L6hAaz+TKQqne5jf5q+rh9iRtA6pqYq0uwk4CgIvELDSPvHpqVCFkp+p8EtMfDgyuLpDxs5z3QDIZfUnCjMjXpuzHLQmN2bndBGQpP3/GsumsVoJfBQ");
+            // Set this code once in App.xaml.cs or application startup
+            SciChart.Charting.Visuals.SciChartSurface.SetRuntimeLicenseKey("WSiYNV5ed+wsx8mTgyFl3zER8MjbGOtnxTU/tt6phukwzhotytEJ0BLdzrUbqHHbBP0ZkBYJIRYT7sVSeNdb/SclGIj8ETTMuPXk75cxYcr4YdWcG19kcvbHWDdZjHjNIibbADkgQG0NBG7ZoutL3KNZZ6jh+TeakVI4NKh8m39XvHGYsZ2q1qEEhsclLhIlGdPiA1nmn/Rvl1w/z/fj7K+ZX00ft/zmAoT/kTBDGeqdKFlb2S272M7TsMkamGLyAjs/b02DcEcJzO4cNgd4F+NKg50/39Gclebg4rhr/jkSobaSKCxYWGz5gz3zcu0Kbn278f0oJdXAPc/jDvROryve5HU4e0axAkQrUeZ1aBfy0XElQL1en+g375hHQlWAh/ebGCS14H/Ma+4oPUp87KeNBd8fpdw0RzxDejw7tjknq56l0ytVl4YNZ8tRMsoJreQLUOx1G9R1QtRm4BjlLGJ4S3ROT6x2QeIiBODdwoCOu/UNPG76AsH4Gx+bCI2ucPxlOpWv3rvAdqFf5YkO9VKT");
         }
     }
 }
