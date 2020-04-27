8fb82aec0f498ebfb8b0f68f03abc649316fd129
Jialin Sun <[sun12764@gmail.com]>
2020-04-27 03:25:56 -0400

    Add Sensor Info

---
+++ GUI/SensorInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    class SensorInfo
+    {
+        private string _name;
+        private int _netWorkID;
+        private string _metaData;
+
+        public string Name { get; }
+        public int NetWorkID { get; }
+        public int SensorID { get; }
+    }
+}
