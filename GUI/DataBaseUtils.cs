f05410c896c400ee349f2ddc0d9c02137cd8ab99
Jialin Sun <[sun12764@gmail.com]>
2020-08-18 22:36:15 -0400

    Change Now all data is saved to a measurement called WSN with different field names.

--- GUI/DataBaseUtils.cs
+++ GUI/DataBaseUtils.cs
@@ -11,43 +11,29 @@ namespace GUI
         public string DataBaseAddress;
         public void PostData(Format.DataPackage dataPackage)
         {
-            string url = DataBaseAddress + "/write?db=WSN&precision=ms";
+            string url = DataBaseAddress + "/write?db=WSN&precision=ms";//Dedabase name is WSN. Time precision is mill seconds.
             WebAPIUtil.HttpPost(url, InfluxDBStringBuilder(dataPackage));
         }
         private string InfluxDBStringBuilder(Format.DataPackage dataPackage)
         {
-            string timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString()+"\n";//Nano second UTCs
-            string result = "";
+            string timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString()+"\n";//Unix UTC time for InfluxDB
+            string result = "WSN,";
+            result += "SensorID=" + dataPackage.SensorID + " ";
             switch (dataPackage.SensorTYpe)
             {
                 case 2:
-                    result += "WindSpeed,";
-                    result += "SensorID=" + dataPackage.SensorID + " ";
-                    result += "value=" + ((dataPackage.WindSpeed / 65536d * 3.3) * (57.6 + 150) / 57.6 * 20).ToString("F3") + " ";
-                    result += timestamp;
-                    result += "WindDirection,";
-                    result += "SensorID=" + dataPackage.SensorID + " ";
-                    result += "value=" + ((dataPackage.WindDirection / 65536d * 3.3) * (57.6 + 150) / 57.6 * 72).ToString("F2") + " ";
-                    result += timestamp;
+                    result += "WindSpeed="+(dataPackage.WindSpeed / 65536d * 3.3 * (57.6 + 150) / 57.6 * 20).ToString("F3") + ",";
+                    result += "WindDirection="+(dataPackage.WindDirection / 65536d * 3.3 * (57.6 + 150) / 57.6 * 72).ToString("F2") + ",";
                     break;
                 case 4:
-                    result += "Pressure,";
-                    result += "SensorID=" + dataPackage.SensorID + " ";
-                    result += "value=" + ((dataPackage.Pressure / 65536d + 0.095) / 0.009 * 10).ToString("F3") + " ";
-                    result += timestamp;
+                    result += "Pressure=" + ((dataPackage.Pressure / 65536d + 0.095) / 0.009 * 10).ToString("F3") + ",";
+                    result += "Battery=" + (dataPackage.Battery / 65536d * 3.3 * 2).ToString("F3") + ",";
                     break;
                 default:
-                    return result;
+                    break;
             }
-            result += "Battery,";
-            result += "SensorID=" + dataPackage.SensorID + " ";
-            result += "value=" + (dataPackage.Battery / 65536d * 3.3 * 2).ToString("F3") + " ";
+            result += "Temperature=" + (((dataPackage.Temperature / 65536d * 3.3 - 0.05) / 0.01 * 1.8) + 32).ToString("F3") + " ";
             result += timestamp;
-            result += "Temperature,";
-            result += "SensorID=" + dataPackage.SensorID + " ";
-            result += "value=" + (((dataPackage.Temperature / 65536d * 3.3 - 0.05) / 0.01 * 1.8) + 32).ToString("F3") + " ";
-            result += timestamp;
-
             return result;
         }
     }
