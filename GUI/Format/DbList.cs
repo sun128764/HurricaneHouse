52e6c7d7fa1fcf07e675ec857d93f784bedea390
Jialin Sun <[sun12764@gmail.com]>
2020-10-15 00:48:14 -0400

    Add Database list

---
+++ GUI/Format/DbList.cs
@@ -0,0 +1,53 @@
+﻿
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GUI.Format
+{
+    class DbInfo
+    {
+        string Url;
+        bool NeedPassWord;
+        string User;
+        string PassWord;
+    }
+    /// <summary>
+    /// Data base list with user name and password.
+    /// </summary>
+    class DbList : List<DbInfo>
+    {
+        static public DbList ReadDbList(string path)
+        {
+            string file = File.ReadAllText(path);
+            try
+            {
+                DbList list = JsonConvert.DeserializeObject<DbList>(file);
+                return list;
+            }
+            catch (JsonSerializationException)
+            {
+                System.Windows.MessageBox.Show("Can not read setting file. Please choose correct file.");
+                return null;
+            }
+        }
+
+        static public void SaveDbList(DbList list)
+        {
+            string setting = JsonConvert.SerializeObject(list, Formatting.Indented);
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Setting files (*.json)|*.json",
+                AddExtension = true,
+                OverwritePrompt = true
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, setting);
+            }
+        }
+    }
+
+    
+}
