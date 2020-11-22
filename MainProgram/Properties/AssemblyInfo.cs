a18794da39fb338f8d6d4f257ccb775285d35b24
Jialin Sun <sun128764@gmail.com>
2020-11-22 16:52:21 +0800

    Added Icon Added software info

--- MainProgram/Properties/AssemblyInfo.cs
+++ MainProgram/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Windows;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("FIT Hurricane House Monitor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Real time monitor for WSN")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Florida Tech")]
+[assembly: AssemblyCompany("Florida Tech")]
+[assembly: AssemblyProduct("Wireless Sensor Network")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -47,5 +47,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
