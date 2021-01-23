c622160b63c46064594d785bab8198ae23a032ed
Jialin Sun <[sun12764@gmail.com]>
2021-01-23 18:23:01 +0800

    Add Humi

--- MainProgram/Format/DataPackage.cs
+++ MainProgram/Format/DataPackage.cs
@@ -30,23 +30,25 @@ namespace Format
         /// <returns>Data package</returns>
         public static DataPackage Decode(byte[] data)
         {
-            DataPackage dataPackage = new DataPackage
-            {
-                TimeSeries = new DateTime[10],
-                SensorID = data[0],
-                SensorTYpe = data[1],
-                Temperature = data[2] << 8,
-                Battery = data[3] << 8,
-                BoardTime = (data[8] << 24) + (data[9] << 16) + (data[10] << 8) + data[11],
-                Pressure = ((data[12] << 8) + data[13]),
-                Time = DateTime.Now,
-                passCrc32 = Crc32Algorithm.IsValidWithCrcAtEnd(data)//The last 4 byte of the data package is CRC32.
-            };
+            DataPackage dataPackage = new DataPackage();
+            dataPackage.TimeSeries = new DateTime[10];
+            dataPackage.SensorID = data[0];
+            dataPackage.SensorTYpe = data[1];
+            dataPackage.Temperature = data[2] << 8;
+            dataPackage.Battery = data[3] << 8;
             if (dataPackage.SensorTYpe == 2)
             {
                 dataPackage.WindSpeed = ((data[4] << 8) + data[5]);
                 dataPackage.WindDirection = ((data[6] << 8) + data[7]);
             }
+            else if (dataPackage.SensorTYpe == 3)
+            {
+                dataPackage.Huminity = ((data[6] << 8) + data[7]);
+            }
+                //dataPackage.Huminity = 0;
+            dataPackage.BoardTime = (data[8] << 24) + (data[9] << 16) + (data[10] << 8) + data[11];
+            dataPackage.Pressure = ((data[12] << 8) + data[13]);
+            dataPackage.Time = DateTime.Now;
 
             dataPackage.DataString += DateTime.Now.ToString("o");
             //dataPackage.DataString += "," + "5001";
@@ -58,8 +60,13 @@ namespace Format
             dataPackage.DataString += "," + dataPackage.WindSpeed.ToString();
             dataPackage.DataString += "," + dataPackage.WindDirection.ToString();
             dataPackage.DataString += "," + dataPackage.Huminity.ToString();
+            dataPackage.passCrc32 = Crc32Algorithm.IsValidWithCrcAtEnd(data);
+            if (dataPackage.SensorTYpe != 4)
+            {
+                return dataPackage;
+            }
+          
 
-            // if (dataPackage.SensorTYpe == 2) return dataPackage;
             dataPackage.PressureList = new int[10];
             int i = 12;
             for (int j = 0; j < 10; j++)
