e87376272e4f5c57cbfdb8a8b9eb02225919ce4d
Jialin Sun <[sun12764@gmail.com]>
2021-01-23 18:11:59 +0800

    Fix CRC32

--- MainProgram/App.xaml.cs
+++ MainProgram/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Diagnostics;
+using System.Threading;
 using System.Windows;
 
 namespace MainProgram
@@ -17,6 +18,12 @@ namespace MainProgram
             }, null);
             // Set this code once in App.xaml.cs or application startup
             SciChart.Charting.Visuals.SciChartSurface.SetRuntimeLicenseKey("WSiYNV5ed+wsx8mTgyFl3zER8MjbGOtnxTU/tt6phukwzhotytEJ0BLdzrUbqHHbBP0ZkBYJIRYT7sVSeNdb/SclGIj8ETTMuPXk75cxYcr4YdWcG19kcvbHWDdZjHjNIibbADkgQG0NBG7ZoutL3KNZZ6jh+TeakVI4NKh8m39XvHGYsZ2q1qEEhsclLhIlGdPiA1nmn/Rvl1w/z/fj7K+ZX00ft/zmAoT/kTBDGeqdKFlb2S272M7TsMkamGLyAjs/b02DcEcJzO4cNgd4F+NKg50/39Gclebg4rhr/jkSobaSKCxYWGz5gz3zcu0Kbn278f0oJdXAPc/jDvROryve5HU4e0axAkQrUeZ1aBfy0XElQL1en+g375hHQlWAh/ebGCS14H/Ma+4oPUp87KeNBd8fpdw0RzxDejw7tjknq56l0ytVl4YNZ8tRMsoJreQLUOx1G9R1QtRm4BjlLGJ4S3ROT6x2QeIiBODdwoCOu/UNPG76AsH4Gx+bCI2ucPxlOpWv3rvAdqFf5YkO9VKT");
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //e.Args为命令行参数
+            //Do something
+            
         }
     }
 }
--- MainProgram/Format/DataPackage.cs
+++ MainProgram/Format/DataPackage.cs
@@ -30,24 +30,26 @@ namespace Format
         /// <returns>Data package</returns>
         public static DataPackage Decode(byte[] data)
         {
-            DataPackage dataPackage = new DataPackage();
-            dataPackage.TimeSeries = new DateTime[10];
-            dataPackage.SensorID = data[0];
-            dataPackage.SensorTYpe = data[1];
-            dataPackage.Temperature = data[2] << 8;
-            dataPackage.Battery = data[3] << 8;
+            DataPackage dataPackage = new DataPackage
+            {
+                TimeSeries = new DateTime[10],
+                SensorID = data[0],
+                SensorTYpe = data[1],
+                Temperature = data[2] << 8,
+                Battery = data[3] << 8,
+                BoardTime = (data[8] << 24) + (data[9] << 16) + (data[10] << 8) + data[11],
+                Pressure = ((data[12] << 8) + data[13]),
+                Time = DateTime.Now,
+                passCrc32 = Crc32Algorithm.IsValidWithCrcAtEnd(data)//The last 4 byte of the data package is CRC32.
+            };
             if (dataPackage.SensorTYpe == 2)
             {
                 dataPackage.WindSpeed = ((data[4] << 8) + data[5]);
                 dataPackage.WindDirection = ((data[6] << 8) + data[7]);
             }
-            else dataPackage.Huminity = 0;
-            dataPackage.BoardTime = (data[8] << 24) + (data[9] << 16) + (data[10] << 8) + data[11];
-            dataPackage.Pressure = ((data[12] << 8) + data[13]);
-            dataPackage.Time = DateTime.Now;
 
             dataPackage.DataString += DateTime.Now.ToString("o");
-            dataPackage.DataString += "," + "5001";
+            //dataPackage.DataString += "," + "5001";
             dataPackage.DataString += "," + dataPackage.SensorID.ToString();
             dataPackage.DataString += "," + dataPackage.SensorTYpe.ToString();
             dataPackage.DataString += "," + dataPackage.BoardTime.ToString();
@@ -56,8 +58,8 @@ namespace Format
             dataPackage.DataString += "," + dataPackage.WindSpeed.ToString();
             dataPackage.DataString += "," + dataPackage.WindDirection.ToString();
             dataPackage.DataString += "," + dataPackage.Huminity.ToString();
-            if (dataPackage.SensorTYpe == 2) return dataPackage;
 
+            // if (dataPackage.SensorTYpe == 2) return dataPackage;
             dataPackage.PressureList = new int[10];
             int i = 12;
             for (int j = 0; j < 10; j++)
@@ -67,10 +69,6 @@ namespace Format
                 dataPackage.DataString += "," + dataPackage.PressureList[j].ToString();
                 i += 2;
             }
-            //byte[] input = new byte[36];
-            //data.CopyTo(input, 0);
-            //Crc32Algorithm.ComputeAndWriteToEnd(input); // CRC32 local test
-            dataPackage.passCrc32 = Crc32Algorithm.IsValidWithCrcAtEnd(data);
             return dataPackage;
         }
     }
